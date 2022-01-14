cf057d0fa2dac6fc89fe31911e2edc4751d491ac
piyush-panpaliya <panpaliyapiyush0@gmail.com>
2022-01-14 20:59:29 +0530

    databse

---
+++ database/README.md
@@ -0,0 +1,3 @@
+# lhd-build-databse
+
+Input your user id if u have one and add anime u watched if u aleready have a id out it in the field and update your list
--- database/main.py
+++ database/main.py
@@ -2,14 +2,44 @@ from faunadb import query as q
 from faunadb.objects import Ref
 from faunadb.client import FaunaClient
 
+class bcolors:
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+
+print(bcolors.WARNING +"Hii this is a anime list maker"+ bcolors.ENDC)
+input1=input(bcolors.BOLD+"Whats ur user token if no token the write na:"+ bcolors.ENDC)
+input2=input(bcolors.BOLD+"write the list of anime seperated by coma and no space: "+ bcolors.ENDC)
+data = input2.split(",")
 client = FaunaClient(
-  secret="fnAEc5nb2IAAxaG4ubeJK-tuGt7Avqhzg49KW3cR",
+  secret="<the api key comes here which i am currently hiding>",
   domain="db.eu.fauna.com",
-  # NOTE: Use the correct domain for your database's Region Group.
   port=443,
   scheme="https"
 )
 
-indexes = client.query(q.paginate(q.indexes()))
+indexes = client.query(q.if_(q.not_(q.exists(q.match(q.index("valid"), input1))),
+    q.do(q.create(q.collection("user-data")["ref"],
+        { 
+          "data": {
+            "token": q.new_id(),
+            "anime": data
+          }   
+        }
+      ),
+      q.select(["data","token"],q.get(q.match(q.index("valid"), input1)))
+    ),
+      q.do( 
+        q.update(
+          q.select(["ref"],q.get(q.match(q.index("valid"), input1))),
+          {
+            "data":{
+              "anime":q.append(q.select(["data","anime"],q.get(q.match(q.index("valid"), input1))),data)
+            }
+          }
+        )
+)))
 
 print(indexes)
